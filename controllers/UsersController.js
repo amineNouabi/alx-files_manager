bbadac810529ef72c0970a8f005190d0cb2d8b16
amineNouabi <aminenouabi9@gmail.com>
2024-12-07 03:33:25 +0100

    Express server Init

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 export default class UsersController {
   static async postNew(req, res) {
@@ -6,10 +7,10 @@ export default class UsersController {
     if (!email) return res.status(400).json({ error: 'Missing email' });
     if (!password) return res.status(400).json({ error: 'Missing password' });
     try {
-      const existingUser = await this.dbClient.users.findOne({ email });
+      const existingUser = await dbClient.users.findOne({ email });
       if (existingUser) return res.status(400).json({ error: 'Already exist' });
       const hashedPassword = sha1(password, { asString: true });
-      const user = await this.dbClient.users.insertOne({ email, password: hashedPassword });
+      const user = await dbClient.users.insertOne({ email, password: hashedPassword });
       return res.status(201).json({ email, password, id: user.id.toString() });
     } catch (err) {
       return res.status(400).json({ error: err.message });
