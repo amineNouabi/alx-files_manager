5c9d96095b5b7bdb3fac18073040dfafc37dfaf1
amineNouabi <aminenouabi9@gmail.com>
2024-12-07 03:08:31 +0100

    Express server Init

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 /**
 * @function bindRoutes - Bind routes to the Express app
  * @param {Express} [app] Express instance
 */
 const bindRoutes = (app) => {
+  /**
+   * App routes
+  */
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
+
+  /**
+   * Users routes
+  */
+  app.post('/users', UsersController.postNew);
 };
 
 export default bindRoutes;
