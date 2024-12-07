1a7d954921fbb7cff9aa79f2014ac210cfe827a7
amineNouabi <aminenouabi9@gmail.com>
2024-12-07 02:50:15 +0100

    Express server Init

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,7 +6,7 @@ export default class AppController {
     res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static getStats(req, res) {
-    res.status(200).json({ users: dbClient.nbUsers(), files: dbClient.nbFiles() });
+  static async getStats(req, res) {
+    res.status(200).json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
   }
 }
--- utils/db.js
+++ utils/db.js
@@ -24,7 +24,6 @@ export class DBClient {
   }
 
   async nbUsers() {
-    console.log(this.db.collection('users').countDocuments());
     return this.db.collection('users').countDocuments();
   }
 
