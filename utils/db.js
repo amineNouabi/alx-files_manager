87fe6ae9c74453774bf98b54386f0a88c1fa4349
amineNouabi <aminenouabi9@gmail.com>
2024-12-07 02:12:15 +0100

    Files Manager Init

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,22 @@
 import { MongoClient } from 'mongodb';
 
-export default class DBClient {
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+export class DBClient {
   constructor() {
-    this.db = MongoClient.connect(process.env.DB_HOST || 'mongodb://localhost:27017', {
-      useNewUrlParser: true,
+    MongoClient.connect(DB_URL, {
       useUnifiedTopology: true,
-    }).then((client) => client.db(process.env.DB_DATABASE || 'files_manager'));
+    }, (err, client) => {
+      if (err) {
+        this.db = false;
+        console.log(err);
+        return;
+      }
+      this.db = client.db(DB_DATABASE);
+    });
   }
 
   isAlive() {
@@ -20,3 +31,7 @@ export default class DBClient {
     return this.db.collection('files').countDocuments();
   }
 }
+
+const dbClient = new DBClient();
+
+export default dbClient;
